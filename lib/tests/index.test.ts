c3004253df0a2a17719dabcd87dcdc4ecbd043bd
hank <akysn564@gmail.com>
2025-06-19 16:15:23 +0800

    refactor: 重構測試條件以排除 JSON 檔案並移除部分hard code #6

--- lib/tests/index.test.ts
+++ lib/tests/index.test.ts
@@ -3,32 +3,14 @@ import path from "path";
 
 import { convert } from "../main";
 
-const vueOnly = ["vue.html", "attr-multiline2.html"];
-const htmlOnly = [
-  "conditional2.html",
-  "conditional3.html",
-  "default-html.html",
-  "html5boilerplate.html",
-  "pre1.html",
-  "pre2.html",
-  "pre3.html",
-  "test.html",
-];
-const testCheckList = {
-  html: vueOnly,
-  vue: htmlOnly,
-};
-
 function generateTests(parser: "html" | "vue") {
   const dir = path.resolve(__dirname, "../../data");
 
   const inputFiles = fs.readdirSync(dir);
 
   return inputFiles
-    .filter((file) => file.includes(".html"))
-    .filter(
-      (file) => !testCheckList[parser].find((name) => file.includes(name)),
-    )
+    .filter((file) => !file.includes(`.${parser == "html" ? "vue" : "html"}.`))
+    .filter((file) => file.endsWith(".html"))
     .map((inputFile) => {
       const htmlPath = path.join(dir, inputFile);
       const html = fs.readFileSync(htmlPath, "utf-8");
@@ -51,6 +33,5 @@ const htmlTests = generateTests("html");
 const vueTests = generateTests("vue");
 
 test.each([...htmlTests, ...vueTests])("%s", (_, html, options) => {
-  // console.log("options:", options);
   expect(convert(html, options)).toMatchSnapshot();
 });
